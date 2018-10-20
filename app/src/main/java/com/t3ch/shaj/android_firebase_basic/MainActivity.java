60eefd97acf92d981b25caf5c0dfafc92d2e2e18
SHAJ <shakilahmedshaj@gmail.com>
2018-10-20 10:48:50 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/android_firebase_basic/MainActivity.java
@@ -0,0 +1,21 @@
+package com.t3ch.shaj.android_firebase_basic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello SHAJ");
+    }
+}
