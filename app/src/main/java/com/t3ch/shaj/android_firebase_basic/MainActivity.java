a533abee5b7bc9b0d2a7b21f582c5588e9dd368b
SHAJ <shakilahmedshaj@gmail.com>
2018-10-20 11:03:46 +0600

    basic writing data

--- app/src/main/java/com/t3ch/shaj/android_firebase_basic/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_firebase_basic/MainActivity.java
@@ -2,6 +2,8 @@ package com.t3ch.shaj.android_firebase_basic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,9 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public void buttonClicked(View view) {
+
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello SHAJ");
+        //myRef.setValue("Hello SHAJ");
+
+        EditText editText = findViewById(R.id.editTextID);
+
+        String message = editText.getText().toString();
+
+        myRef.setValue(message);
+
+
     }
+
+
 }
