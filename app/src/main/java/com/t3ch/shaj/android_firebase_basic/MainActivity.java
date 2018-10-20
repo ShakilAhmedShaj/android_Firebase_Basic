a53b4301f955e5277b53cacadf340902e2773d5c
SHAJ <shakilahmedshaj@gmail.com>
2018-10-20 11:21:59 +0600

    basic reading data

--- app/src/main/java/com/t3ch/shaj/android_firebase_basic/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_firebase_basic/MainActivity.java
@@ -1,14 +1,25 @@
 package com.t3ch.shaj.android_firebase_basic;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+        textView = findViewById(R.id.TV_ID);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String value = dataSnapshot.getValue(String.class);
+
+                textView.setText(value);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
     public void buttonClicked(View view) {
 
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        //myRef.setValue("Hello SHAJ");
 
         EditText editText = findViewById(R.id.editTextID);
 
